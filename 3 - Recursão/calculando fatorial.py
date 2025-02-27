bc081fecd83bd8fcf55d0ea319103210df14b580
Antonio Sachinski <as.sachinski@gmail.com>
2025-02-27 02:50:36 -0300

    adicionando nova função

---
+++ 3 - Recursão/calculando fatorial.py
@@ -0,0 +1,13 @@
+# função que identifica a posição de um item em um array
+# execício proposto no terceiro capítulo, com o intuito de explicar a recursão, conceito onde uma função chama a si mesma para resolver um problema
+
+numero = int(input("Digite um número para calcular o fatorial: "))
+# estrutura básica de uma função recursiva:
+def fatorial(numero):
+  if (numero == 1): 
+    return 1 # caso Base: A condição de parada, onde a função não se chama novamente
+    # sem o caso base, a recursão continuaria infinitamente 
+  else:
+    return numero * fatorial(numero - 1) # chamada Recursiva: A parte onde a função se chama novamente
+
+print("O fatorial de", numero, "é", fatorial(numero))
