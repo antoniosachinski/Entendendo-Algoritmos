532122978cb0013526c2ca3fa05f50f6ef2ef15c
Antonio Sachinski <as.sachinski@gmail.com>
2025-03-01 17:31:19 -0300

    Comentando

--- 4 - Quicksort/quicksort.py
+++ 4 - Quicksort/quicksort.py
@@ -1,16 +1,24 @@
 def quick(lista):
-  if len(lista) < 2:
-    return lista
-  else:
-    pivo = lista[0]
-    menores = []
-    maiores = []
-    for item in lista[1:]: 
-      if item < pivo:
-        menores.append(item)
-      else:
-        maiores.append(item)
-  return quick(menores) + [pivo] + quick(maiores)
+    if len(lista) < 2: # caso base da recursão: se a lista tiver menos de 2 elementos
+        return lista # não há necessidade de ordenar, então simplesmente retorna a lista.
+    else:
+        # escolhendo o primeiro item da lista como o pivô
+        pivo = lista[0]
+        menores = []
+        maiores = []
 
-a = [0,1,2,3,4,5]
+        # laço para percorrer os elementos da lista (exceto o pivô)
+        for item in lista[1:]:
+            # se o item for menor que o pivô, ele vai para a lista 'menores'
+            if item < pivo:
+                menores.append(item)
+            # caso contrário, ele vai para a lista 'maiores'
+            else:
+                maiores.append(item)
+        
+        # a recursão chama o quick novamente para ordenar as listas 'menores' e 'maiores'
+        # depois organiza o pivô no meio e retorna a lista ordenada
+        return quick(menores) + [pivo] + quick(maiores)
+
+a = [0, 1, 2, 3, 4, 5]
 print(quick(a))
