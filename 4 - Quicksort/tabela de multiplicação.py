e06c03e6f34ca5bbabca123106811e301bdaf8d9
Antonio Sachinski <as.sachinski@gmail.com>
2025-03-06 21:53:45 -0300

    Create tabela de multiplicação.py

---
+++ 4 - Quicksort/tabela de multiplicação.py
@@ -0,0 +1,10 @@
+# função que cria uma tabela de multiplicação com todos os elementos do array
+
+def escreverLista(lista): 
+  for item in lista: # laço externo
+    for item2 in lista: # laço interno
+      print(item2 * item, end="\t")  # imprime o resultado, com tabulação 
+    print()  # quebra de linha após cada linha
+
+lista = [0,1,2,3,4,5,6,7,8,9]
+escreverLista(lista)
