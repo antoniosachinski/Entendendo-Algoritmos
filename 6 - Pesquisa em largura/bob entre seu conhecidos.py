73ebc21845c77eb98b7010b30645e41e44656648
Antonio Sachinski <as.sachinski@gmail.com>
2025-03-11 22:21:48 -0300

    melhorando a função
    adicionando o grupo dos já verificados, poupando trabalho de reverificação e evitando loops (gerados caso você esteja dentro da rede de alguém da sua rede)

--- 6 - Pesquisa em largura/bob entre seu conhecidos.py
+++ 6 - Pesquisa em largura/bob entre seu conhecidos.py
@@ -4,18 +4,24 @@
 
 from collections import deque # importando a fila
 
-grafo = {"bob" : ["any", "clara", "alice"], "any": ["jao", "bia", "tom"], "alice" : ["tim", "bruna", "ant", "aa"], "clara" : []}
+grafo = {"voce" : ["any", "clara", "alice"], "any": ["jao", "bia", "tom"], "alice" : ["tim", "bob", "bruna", "ant", "aa"], "clara" : []}
 
 def pesquisaLargura(grafo):
-  fila = deque(grafo["bob"]) # 1. crie uma lista contendo todas a pessoas a serem classificadas
+  fila = deque(grafo["voce"]) # 1. crie uma lista contendo todas a pessoas a serem classificadas
+  verificados = set() # lista para os já verificados
+  
   while len(fila) > 0:
     pessoa = fila[0]
     fila.popleft() # 2. retire uma pessoa da fila
-
-    if (pessoa == "bob"): # 3. confira se ela é o bob
-      print("Você achou o bob!") # 3.2 sim!
-    else:
-      fila.extend(grafo.get(pessoa, [])) # 3.3 não! adiciona a rede dessa pessoa
+    
+    if pessoa not in verificados:  # Vvê se a pessoa já foi verificada
+      if (pessoa == "bob"): # 3. confira se ela é o bob
+        print("Você achou o bob!") # 3.2 sim!
+        return
+    verificados.add(pessoa) # vai para a lista verificados
+    fila.extend(grafo.get(pessoa, [])) # 3.3 não! adiciona a rede dessa pessoa
   # 4. repita
+  
+  print("bob não encontrado :(")
 
 pesquisaLargura(grafo)
